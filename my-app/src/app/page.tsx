381a4abbc0a8d7c2bf6e4f0612ebe35aab81117d
Nicholas Soh <nicholassdl660@gmail.com>
2024-10-08 11:38:35 +0800

    navbar and simple card

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,30 @@
 //import Image from "next/image";
 
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <div>
+      Hello World!
+      <Card>
+        <CardHeader>
+          <p>Hi there, My name is Nicholas!</p>
+        </CardHeader>
+        <CardDescription>
+          I am awesome man
+        </CardDescription>
+        <CardContent>
+          <div className = "flex flex-row gap-2">
+            <p className="font-bold"> 
+              Name:
+            </p> 
+            Nicholas
+            <div className = "flex flex-row gap-2"> <p className = "font-bold">Major:</p>Software Engineering
+            </div>
+             
+          </div>
+        </CardContent>
+      </Card>  
+    </div>
   );
 }
